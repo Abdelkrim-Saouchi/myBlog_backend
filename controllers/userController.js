94fda10c634cc2cc84e807e02488cb6cc1c8e953
Krimothiazine <krimouv1995@gmail.com>
2024-02-05 17:35:25 +0100

    Add login input validation

--- controllers/userController.js
+++ controllers/userController.js
@@ -32,26 +32,35 @@ exports.userSignUp = [
   },
 ];
 
-exports.userLogin = async (req, res, next) => {
-  const { email, password } = req.body;
-  try {
-    const user = await User.findOne({ email: email }).exec();
-    if (email === user.email && password === user.password) {
-      const options = {};
-      options.expiresIn = '2 days';
-      const secret = process.env.SECRET;
-      const token = jwt.sign(
-        { id: user._id, username: user.username, admin: false },
-        secret,
-        options
-      );
-      return res.json({
-        message: 'Auth passed',
-        token: token,
-      });
+exports.userLogin = [
+  body('email', 'invalid email').trim().isLength({ min: 1 }).escape(),
+  body('password', 'invalid password').trim().isLength({ min: 1 }).escape(),
+  async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-    return res.status(401).json({ message: 'Auth failed!' });
-  } catch (err) {
-    next(err);
-  }
-};
+
+    const { email, password } = req.body;
+    try {
+      const user = await User.findOne({ email: email }).exec();
+      if (email === user.email && password === user.password) {
+        const options = {};
+        options.expiresIn = '2 days';
+        const secret = process.env.SECRET;
+        const token = jwt.sign(
+          { id: user._id, username: user.username, admin: false },
+          secret,
+          options
+        );
+        return res.json({
+          message: 'Auth passed',
+          token: token,
+        });
+      }
+      return res.status(401).json({ message: 'Auth failed!' });
+    } catch (err) {
+      next(err);
+    }
+  },
+];
