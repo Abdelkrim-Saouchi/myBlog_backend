0be0946f296e6ce7ecce61c9929177e9f7619df9
Krimothiazine <krimouv1995@gmail.com>
2024-02-20 17:48:00 +0100

    Refactor sign up controller to handle validation and encrypted pwd

--- controllers/authorSingupController.js
+++ controllers/authorSingupController.js
@@ -1,17 +1,49 @@
 const Author = require('../models/author');
+const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 
-exports.authorSingUp = async (req, res, next) => {
-  const author = new Author({
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    password: req.body.password,
-  });
+exports.authorSingUp = [
+  body('firstName', 'First name must not be empty')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('lastName', 'Last name must not be empty')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('email', 'email must not be empty').trim().isLength({ min: 1 }).escape(),
+  body('password', 'password must not be empty')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('confirmation')
+    .custom((value, { req }) => value === req.body.password)
+    .withMessage('Passwords do not match!')
+    .escape(),
+  async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res
+        .status(400)
+        .json({ message: 'Invalid inputs', errors: errors.array() });
+    }
 
-  try {
-    await author.save();
-    res.json(author);
-  } catch (err) {
-    next(err);
-  }
-};
+    bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+      if (err) return next(err);
+
+      const author = new Author({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: hashedPassword,
+      });
+
+      try {
+        await author.save();
+        res.json(author);
+      } catch (err) {
+        next(err);
+      }
+    });
+  },
+];
