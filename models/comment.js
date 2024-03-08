bfbc0aaac62fdcdb773da81d5ec38e66daf15575
Krimothiazine <krimouv1995@gmail.com>
2024-03-08 21:52:35 +0100

    Make comment ref dynamic

--- models/comment.js
+++ models/comment.js
@@ -1,22 +1,27 @@
-const mongoose = require('mongoose');
-const { format } = require('date-fns');
+const mongoose = require("mongoose");
+const { format } = require("date-fns");
 
 const Schema = mongoose.Schema;
 
 const CommentSchema = new Schema(
   {
-    post: { type: Schema.Types.ObjectId, ref: 'Post', required: true },
-    author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    post: { type: Schema.Types.ObjectId, ref: "Post", required: true },
+    author: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      refPath: "userModel",
+    },
+    userModel: { type: String, required: true, enum: ["User", "Author"] },
     content: { type: String, required: true },
   },
-  { timestamps: true }
+  { timestamps: true },
 );
 
-CommentSchema.virtual('creationDate').get(function () {
-  const formattedDate = format(this.createdAt, 'MMM dd');
+CommentSchema.virtual("creationDate").get(function () {
+  const formattedDate = format(this.createdAt, "MMM dd");
   return formattedDate;
 });
 
-CommentSchema.set('toJSON', { virtuals: true });
+CommentSchema.set("toJSON", { virtuals: true });
 
-module.exports = mongoose.model('Comment', CommentSchema);
+module.exports = mongoose.model("Comment", CommentSchema);
