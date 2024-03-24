3c7aea20efe132a5c4cf4ba77a4533a393f82f29
Krimothiazine <krimouv1995@gmail.com>
2024-03-24 22:32:25 +0100

    Add better code status and handle edge cases

--- controllers/likeController.js
+++ controllers/likeController.js
@@ -1,12 +1,12 @@
-const Like = require('../models/like');
-const Post = require('../models/post');
+const Like = require("../models/like");
+const Post = require("../models/post");
 
 exports.createLike = async (req, res, next) => {
   try {
     const oldLike = await Like.findOne({ owner: req.user._id }).exec();
 
     if (oldLike) {
-      return res.status(403).json({ message: 'already liked' });
+      return res.status(403).json({ message: "already liked" });
     }
     const like = new Like({
       owner: req.user._id,
@@ -15,7 +15,7 @@ exports.createLike = async (req, res, next) => {
     const post = await Post.findById(req.params.postId).exec();
     post.likes = [...post.likes, like];
     await post.save();
-    res.json({ message: 'like created', likeId: like._id });
+    res.status(201).json({ message: "like created", likeId: like._id });
   } catch (err) {
     next(err);
   }
@@ -25,9 +25,9 @@ exports.getLike = async (req, res, next) => {
   try {
     const like = await Like.findById(req.params.likeId).exec();
     if (like) {
-      return res.json({ message: 'like already exists', owner: like.owner });
+      return res.json({ message: "like already exists", owner: like.owner });
     }
-    return res.status(404).json({ message: 'like not found' });
+    return res.status(404).json({ message: "like not found" });
   } catch (err) {
     next(err);
   }
@@ -35,13 +35,19 @@ exports.getLike = async (req, res, next) => {
 
 exports.deleteLike = async (req, res, next) => {
   try {
-    await Like.findByIdAndDelete(req.params.likeId);
+    const deletedLike = await Like.findByIdAndDelete(req.params.likeId);
+    if (!deletedLike) {
+      return res.status(409);
+    }
     const post = await Post.findById(req.params.postId).exec();
 
     post.likes = post.likes.filter((id) => id.toString() !== req.params.likeId);
 
-    await post.save();
-    res.json({ message: 'Like deleted' });
+    const savedPost = await post.save();
+    if (!savedPost) {
+      return res.status(409);
+    }
+    res.status(204);
   } catch (err) {
     next(err);
   }
@@ -51,9 +57,9 @@ exports.getLikedStatus = async (req, res, next) => {
   try {
     const like = await Like.findOne({ owner: req.user._id }).exec();
     if (!like) {
-      return res.status(404).json({ message: 'Like not found' });
+      return res.status(404).json({ message: "Like not found" });
     }
-    res.json({ message: 'Like exists', likeId: like._id });
+    res.json({ message: "Like exists", likeId: like._id });
   } catch (err) {
     next(err);
   }
